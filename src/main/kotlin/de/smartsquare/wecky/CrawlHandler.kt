3e04af5b4e16cff30c0cbb56b927b9a8f01fb5aa
Daniel Rosowski <rosowski@smartsquare.de>
2018-06-24 13:24:56 +0200

    Use standard dynamodb client for eu_central

--- src/main/kotlin/de/smartsquare/wecky/CrawlHandler.kt
+++ src/main/kotlin/de/smartsquare/wecky/CrawlHandler.kt
@@ -1,7 +1,7 @@
 package de.smartsquare.wecky
 
-import com.amazonaws.client.builder.AwsClientBuilder
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
@@ -20,14 +20,14 @@ class CrawlHandler : RequestStreamHandler {
     override fun handleRequest(input: InputStream, output: OutputStream, ctx: Context?) {
         val website = mapper.readValue(input, Website::class.java)
 
-
-        val amazonDynamoDB = AmazonDynamoDBClient.builder()
-                .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-east-1"))
+        val amazonDynamoDB = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.EU_CENTRAL_1)
                 .build()
         val dynamo = DynamoDbClient(amazonDynamoDB)
+
+        val crawler = WebsiteCrawler()
         val sqs = SqsPublisher()
         val tracker = WebsiteTracker(dynamo, sqs)
-        val crawler = WebsiteCrawler()
 
         val hashedWebsite = crawler.crawlPage(website)
         tracker.track(website, hashedWebsite)
