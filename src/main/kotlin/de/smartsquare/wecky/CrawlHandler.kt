fa3184b07817c9ab5c0a1ecb17cc7413c2c50189
Daniel Rosowski <rosowski@smartsquare.de>
2018-06-24 19:46:33 +0200

    Settings for local test with sam

--- src/main/kotlin/de/smartsquare/wecky/CrawlHandler.kt
+++ src/main/kotlin/de/smartsquare/wecky/CrawlHandler.kt
@@ -1,6 +1,8 @@
 package de.smartsquare.wecky
 
+import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.regions.Regions
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler
@@ -20,9 +22,20 @@ class CrawlHandler : RequestStreamHandler {
     override fun handleRequest(input: InputStream, output: OutputStream, ctx: Context?) {
         val website = mapper.readValue(input, Website::class.java)
 
-        val amazonDynamoDB = AmazonDynamoDBClientBuilder.standard()
-                .withRegion(Regions.EU_CENTRAL_1)
-                .build()
+        // used for local test with sam
+        val isLocalTest = System.getenv("WECKY_LOCAL")?.isNotEmpty() ?: false
+        val amazonDynamoDB =
+                if (isLocalTest) {
+                    System.setProperty("aws.accessKeyId", "test1")
+                    System.setProperty("aws.secretKey", "test231")
+                    AmazonDynamoDBClient.builder()
+                            .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration("http://host.docker.internal:8000", "us-east-1"))
+                            .build()
+                } else {
+                    AmazonDynamoDBClientBuilder.standard()
+                            .withRegion(Regions.EU_CENTRAL_1)
+                            .build()
+                }
         val dynamo = DynamoDbClient(amazonDynamoDB)
 
         val crawler = WebsiteCrawler()
