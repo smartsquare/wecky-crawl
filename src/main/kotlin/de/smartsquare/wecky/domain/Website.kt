16c9277eb054b5912365833497e740cd77b4779f
Daniel Rosowski <rosowski@smartsquare.de>
2018-06-24 13:10:13 +0200

    Ignore unknown fields by default

--- src/main/kotlin/de/smartsquare/wecky/domain/Website.kt
+++ src/main/kotlin/de/smartsquare/wecky/domain/Website.kt
@@ -1,3 +1,6 @@
 package de.smartsquare.wecky.domain
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Website(val id: String, val url: String)
