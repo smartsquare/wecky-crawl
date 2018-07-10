35295ba15c44d1d2860625d92efe7da8cd34b107
Daniel Rosowski <rosowski@smartsquare.de>
2018-07-10 14:35:47 +0200

    Refactoring

--- src/main/kotlin/de/smartsquare/wecky/CrawlHandler.kt
+++ src/main/kotlin/de/smartsquare/wecky/CrawlHandler.kt
@@ -2,6 +2,7 @@ package de.smartsquare.wecky
 
 import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.regions.Regions
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.lambda.runtime.Context
@@ -25,22 +26,7 @@ class CrawlHandler : RequestStreamHandler {
     override fun handleRequest(input: InputStream, output: OutputStream, ctx: Context?) {
         val website = mapper.readValue(input, Website::class.java)
 
-        // used for local test with sam
-        val dyndbLocal = getEnv()
-        val amazonDynamoDB =
-                if (dyndbLocal?.isNotEmpty() == true) {
-                    log.info("Triggered local dev mode using local DynamoDB at [$dyndbLocal]")
-                    System.setProperty("aws.accessKeyId", "key")
-                    System.setProperty("aws.secretKey", "key2")
-                    AmazonDynamoDBClient.builder()
-                            .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(dyndbLocal, "eu-central-1"))
-                            .build()
-                } else {
-                    log.info("Using production DynamoDB at eu_central_1")
-                    AmazonDynamoDBClientBuilder.standard()
-                            .withRegion(Regions.EU_CENTRAL_1)
-                            .build()
-                }
+        val amazonDynamoDB = determineDynamoDB()
         val hashedWebsiteRepo = HashedWebsiteRepository(amazonDynamoDB)
 
         val crawler = WebsiteCrawler()
@@ -54,4 +40,21 @@ class CrawlHandler : RequestStreamHandler {
     }
 
     private fun getEnv() = System.getenv("DYNDB_LOCAL") ?: System.getProperty("DYNDB_LOCAL")
+
+    private fun determineDynamoDB(): AmazonDynamoDB {
+        val dyndbLocal = getEnv()
+        return if (dyndbLocal?.isNotEmpty() == true) {
+            log.info("Triggered local dev mode using local DynamoDB at [$dyndbLocal]")
+            System.setProperty("aws.accessKeyId", "key")
+            System.setProperty("aws.secretKey", "key2")
+            AmazonDynamoDBClient.builder()
+                    .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(dyndbLocal, "eu-central-1"))
+                    .build()
+        } else {
+            log.info("Using production DynamoDB at eu-central-1")
+            AmazonDynamoDBClientBuilder.standard()
+                    .withRegion(Regions.EU_CENTRAL_1)
+                    .build()
+        }
+    }
 }
